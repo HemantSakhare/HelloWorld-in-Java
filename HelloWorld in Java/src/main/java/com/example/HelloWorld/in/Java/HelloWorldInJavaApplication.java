8cf42bdfaadbe124eaeb15fb16cc2ebd28ecf67d
HemantSakhare <sakhare.hemant@gmail.com>
2019-05-05 13:27:46 +0530

    First Java Program

---
+++ HelloWorld in Java/src/main/java/com/example/HelloWorld/in/Java/HelloWorldInJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.HelloWorld.in.Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldInJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldInJavaApplication.class, args);
+	}
+
+}
